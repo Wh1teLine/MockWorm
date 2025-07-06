ab864c08a32c0484dfc0418d2f83341fac156956
Alfin <akuntumbalwin777@gmail.com>
2025-07-06 19:56:11 +0700

    Update MockWorm.py

--- MockWorm.py
+++ MockWorm.py
@@ -3,26 +3,21 @@ import shutil
 
 def duplicate_files_recursive(folder):
     try:
-        # List isi folder sekarang
         items = os.listdir(folder)
         
         for item in items:
             path = os.path.join(folder, item)
             
             if os.path.isfile(path):
-                # File: buat duplikat dengan nama file_copy
                 new_name = item + "_copy"
                 new_path = os.path.join(folder, new_name)
                 if not os.path.exists(new_path):
                     shutil.copy2(path, new_path)
             
             elif os.path.isdir(path):
-                # Folder: duplikasi folder baru dengan _copy
                 new_folder = path + "_copy"
                 if not os.path.exists(new_folder):
                     shutil.copytree(path, new_folder)
-                
-                # Lakukan rekursi ke folder asli (bukan ke duplikat)
                 duplicate_files_recursive(path)
 
     except Exception as e:
